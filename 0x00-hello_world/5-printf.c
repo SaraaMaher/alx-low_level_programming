086238b4d90c0ff95980ad925116dcfec1a88ce2
SaraaMaher <saraamaher13@gmail.com>
2022-12-08 02:13:40 -0800

    6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *Main.c
+ * main - Entry point
  *
- * return 0
+ * Return: Always 0 (Success)
  *
  */
 
 int main(){
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
