469fd372b309d8125b0cc717974230c46335eb03
SaraaMaher <saraamaher13@gmail.com>
2022-12-09 05:03:49 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all num
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+putchar((i % 10) + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
