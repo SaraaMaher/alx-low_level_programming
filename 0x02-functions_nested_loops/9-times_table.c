ae7a1406efdb0b1b1d8a951bc490f4a09367851c
SaraaMaher <saraamaher13@gmail.com>
2022-12-13 01:34:45 -0800

    Done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,10 @@ void times_table(void)
 int i, j, x;
 for (i = 0 ; i < 10 ; i++)
 {
-for (j = 0 ; j < 10 ; j++)
+putchar('0');
+putchar(',');
+putchar(' ');
+for (j = 1 ; j < 10 ; j++)
 {
 x = i * j;
 if ((x / 10) > 0)
