34fc71de017b87d0f7213aa485d5ed18043d511f
SaraaMaher <saraamaher13@gmail.com>
2022-12-18 19:53:38 -0800

    1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer
+ * Return: int
+ */
+int _strlen(char *s)
+{
+int l = 0;
+while (*s != null)
+{
+l++;
+s++;
+}
+}
