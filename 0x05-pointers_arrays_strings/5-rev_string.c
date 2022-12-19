fd22ac230a5bbffc98234e95710730148ff8e9c3
SaraaMaher <saraamaher13@gmail.com>
2022-12-18 20:28:57 -0800

    1

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * rev_string - reverses a string
+ * @s: pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int l, i;
+char *v;
+for (l = 0 ; s[l] != '\0' ; l++)
+{
+}
+l--;
+for (i = 0 ; i < l ; i++)
+{
+v[i] = s[l];
+l--;
+}
+s = v;
+}
