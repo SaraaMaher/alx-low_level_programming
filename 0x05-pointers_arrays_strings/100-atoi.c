6dacc02659724cf787ca983959045044f356f3f5
SaraaMaher <saraamaher13@gmail.com>
2022-12-19 03:00:00 -0800

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,11 +6,21 @@
  */
 int _atoi(char *s)
 {
-int r, i;
+int r, i, s;
 r = 0;
-for (i = 0 ; s[i] != 0 ; i++)
+s = 1;
+if (s[0] = '-')
 {
+i = 1;
+s = -1;
+}
+else
+i = 0;
+for (i ; s[i] != 0 ; i++)
+{
+if (s[i] >= '0' || s[i] <= '9')
 r = r * 10 + s[i] - '0';
 }
+r = r * s;
 return (r);
 }
