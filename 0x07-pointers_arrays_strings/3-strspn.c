9afb883a237533b8889b94d1b5f6bafd6530f335
SaraaMaher <saraamaher13@gmail.com>
2023-01-03 01:54:31 -0800

    0

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer
+ * @accept: pointer
+ * Return: uint
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int r = 0;
+while (*s != '\0')
+{
+int i = 0;
+for (; accept[i] != '\0' ; i++)
+{
+if (*s == accept[i])
+r++;
+}
+s++;
+}
+return (r);
+}
