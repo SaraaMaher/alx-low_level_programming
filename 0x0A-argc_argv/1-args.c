9058da9c42fae587c0e90cad26d005a52fac8246
SaraaMaher <saraamaher13@gmail.com>
2023-01-06 02:52:19 -0800

    DONE

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print arg
+ * @argc: size
+ * @argv: array
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+argv = argv;
+printf("%d\n", argc - 1);
+return (0);
+}
