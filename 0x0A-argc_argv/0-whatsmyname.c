4b25542079082dd6031f080ffb8045405bc8e2bb
SaraaMaher <saraamaher13@gmail.com>
2023-01-06 02:55:08 -0800

    DONE

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints name
+ * @argc: size
+ * @argv: array
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argv[0]);
+return (0);
+}
