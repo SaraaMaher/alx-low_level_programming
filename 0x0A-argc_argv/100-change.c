f0b54ce154c691cdd5a8532560c38afa5d9052a3
SaraaMaher <saraamaher13@gmail.com>
2023-01-06 03:45:08 -0800

    DONE

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - min num
+ * @argc: size
+ * @argv: array
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+int coins = atoi(argv[1]);
+int r = 0;
+if (coins < 0)
+{
+printf("0\n");
+return (0);
+}
+int i;
+while (coins)
+{
+if (coins >= 25)
+i = 25;
+else if (coins >= 10)
+i = 10;
+else if (coins >= 5)
+i = 5;
+else if (coins >=2)
+i = 2;
+else
+i = 1;
+r += coins / i;
+coins = coins % i;
+}
+printf("%d\n", r);
+return (0);
+}
