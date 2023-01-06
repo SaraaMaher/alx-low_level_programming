122710aa1110e71c2b301bd07237b572b2c9a990
SaraaMaher <saraamaher13@gmail.com>
2023-01-06 03:32:33 -0800

    DONE

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,16 +8,17 @@
  */
 int main(int argc, char *argv[])
 {
-int i, j, r = 0;
+int i, j, r;
+r = 0;
 if (argc <= 1)
 printf("0\n");
 else
 {
-for (i = 1; i < argc - 1; i++)
+for (i = 1; i < argc ; i++)
 {
 for (j = 0; argv[i][j] ; j++)
 {
-if (!isnumber(argv[i][j]))
+if (isnumber(argv[i][j]) == 0)
 {
 printf("Error\n");
 return (1);
@@ -25,6 +26,7 @@ return (1);
 }
 r += atoi(argv[i]);
 }
+printf("%d\n", r);
 }
 }
 return (0);
