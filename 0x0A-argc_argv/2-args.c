a9ea0ec1ddbb91292c9a5a65c95762c673db3626
SaraaMaher <saraamaher13@gmail.com>
2023-01-06 03:01:45 -0800

    DONE

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - rints all arguments it receives
+ * @argc: size
+ * @argv: array
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0 ; i < argc ; i++)
+printf("%s\n", arg[i]);
+return (0);
+}
