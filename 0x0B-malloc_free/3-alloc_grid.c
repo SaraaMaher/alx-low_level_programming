44de789b7ddf458c8f570b92690685d20e95e7d5
SaraaMaher <saraamaher13@gmail.com>
2023-01-09 02:12:05 -0800

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,6 +13,8 @@ int **grid;
 if (width <= 0 || height <= 0)
 return (NULL);
 grid = malloc(sizeof(int *) * height);
+if (grid == NULL)
+return (NULL);
 for (row = 0 ; row < height ; row++)
 grid[row] = malloc(sizeof(int) * width);
 
