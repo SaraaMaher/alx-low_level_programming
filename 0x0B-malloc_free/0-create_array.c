c68931e4085a1a5e72c63b7cddd5095b78d1823f
SaraaMaher <saraamaher13@gmail.com>
2023-01-09 01:19:05 -0800

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,10 +7,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
+unsigned int i;
+char *s;
 if (size == 0)
 return (NULL);
-int i;
-char *s = malloc(sizeof(c) * size);
+
+s = malloc(sizeof(c) * size);
 for (i = 0 ; i < size ; i++)
 s[i] = c;
 return (s);
