4fd710141fbf6d451699df82d9dadfd27f703f7f
SaraaMaher <saraamaher13@gmail.com>
2023-01-09 01:45:34 -0800

    1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,10 @@ char *str_concat(char *s1, char *s2)
 {
 int l1, l2, i;
 char *s;
-
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 l1 = strlen(s1);
 l2 = strlen(s2);
 s = malloc(sizeof(char) * (l1+l2) + 1);
